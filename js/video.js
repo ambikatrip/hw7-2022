ba1d2f631d087ea10f662a5d6746884f8c236dfc
ambikatrip <98373190+ambikatrip@users.noreply.github.com>
2022-11-08 23:58:34 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -55,18 +55,6 @@ document.querySelector("#mute").addEventListener("click", function() {
 });
 
 
-$('#audio-control').click(function(){
-    if( $("#myVideo").prop('muted') ) {
-          $("#myVideo").prop('muted', false);
-          $(this).text('Mute');
-      // or toggle class, style it with a volume icon sprite, change background-position
-    } else {
-      $("#myVideo").prop('muted', true);
-      $(this).text('Unmute');
-    }
-});
-
-
 
 document.querySelector("#slider").addEventListener("click", function() {
 	console.log(this.value);
